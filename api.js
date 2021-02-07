300ed4212f83da17162b32374982afd1094ab7e5
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-02-07 21:58:40 +0600

    done preivious work using forEach

--- api.js
+++ api.js
@@ -8,28 +8,43 @@ const displayMeals = mealsObj =>{
     // console.log(mealsArray);
 
     const mealsDiv = document.getElementById('meals-div');
-    for (let i = 0; i < mealsArray.length; i++) {
-        const meal = mealsArray[i];
+    // for (let i = 0; i < mealsArray.length; i++) {
+    //     const meal = mealsArray[i];
+    //     const mealDiv = document.createElement('div');
+    //     mealDiv.className = 'meal'
+    //     // const image = document.createElement('img');
+    //     // image.innerText = meal.strMealThumb;
+    //     // mealDiv.appendChild(image);
+
+    //     // const mealTitle = document.createElement('h3');
+    //     // mealTitle.innerText = meal.strMeal;
+    //     // mealDiv.appendChild(mealTitle);
+    //     // // mealDiv.innerText = meal.strMeal;
+    //     // //mealsDiv.appendChild(mealDiv);
+
+    //     //  //Done previous works more visually
+    //     const mealInfo = `
+    //         <img class='food-image' src="${meal.strMealThumb}">
+    //         <h3>${meal.strMeal} </h3>
+    //     `
+    //     mealDiv.innerHTML = mealInfo;
+    //     mealsDiv.appendChild(mealDiv);
+
+
+    
+     // console.log(meal.strMeal);
+    // }
+    // Trying above using forEach
+    mealsArray.forEach(meal => {
+        //  const meal = mealsArray[i];
         const mealDiv = document.createElement('div');
-
-        // const image = document.createElement('img');
-        // image.innerText = meal.strMealThumb;
-        // mealDiv.appendChild(image);
-
-        // const mealTitle = document.createElement('h3');
-        // mealTitle.innerText = meal.strMeal;
-        // mealDiv.appendChild(mealTitle);
-        // // mealDiv.innerText = meal.strMeal;
-        // mealsDiv.appendChild(mealDiv);
-
+        mealDiv.className = 'meal';
         const mealInfo = `
-            <img src="${meal.strMealThumb}">
+            <img class='food-image' src="${meal.strMealThumb}">
             <h3>${meal.strMeal} </h3>
         `
         mealDiv.innerHTML = mealInfo;
         mealsDiv.appendChild(mealDiv);
+    });
 
-
-        console.log(meal.strMeal);
-    }
 }
